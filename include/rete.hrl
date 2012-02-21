1ffd0d8bc5e56465a902b7cc48ebb4638ca14f06
Vyacheslav Vorobyov <vjache@gmail.com>
2012-02-21 19:42:07 +0400

    Add bnodes map containing substitutions.

--- include/rete.hrl
+++ include/rete.hrl
@@ -4,24 +4,25 @@
 
 % An alpha node definition
 -record(anode, 
-        {id :: integer(),
+        {id             :: integer(),
          wme_constraint :: fun( (term()) -> boolean() ), 
-         bnode_ids :: [integer()]}).
+         bnode_ids      :: [integer()]}).
 % A beta node definition
 -record(bnode, 
-        {id :: integer(),
+        {id             :: integer(),
          token_constraint :: fun( (term()) -> boolean() ),
-         left_key_fun :: fun( (term()) -> term() ),
-         right_key_fun :: fun( (term()) -> term() ),
-         pnodes=[]:: [integer()],
-         bnode_ids :: [integer()] }).
+         left_key_fun   :: fun( (term()) -> term() ),
+         right_key_fun  :: fun( (term()) -> term() ),
+         pnodes=[]      :: [integer()],
+         bnode_ids      :: [integer()] }).
 % An output node definition
 -record(pnode,
-        {id :: integer(), 
-         token_format :: fun( ([ [T] ]) -> [T] ),
-         bnode_ids :: [integer()]}).
+        {id             :: integer(), 
+         token_format   :: fun( ([ [T] ]) -> [T] ),
+         bnode_ids      :: [integer()]}).
 % A rete spec
 -record(rete, 
-        {anodes=[]:: [#anode{}],
-         bnodes=[]:: [#bnode{}],
-         pnodes=[]:: [#pnode{}]}).
+        {anodes=[]      :: [#anode{}],
+         bnodes=[]      :: [#bnode{}],
+         pnodes=[]      :: [#pnode{}],
+         bnodes_map=[]  :: [#bnode{}]}).
